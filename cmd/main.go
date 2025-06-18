a720c47afaa2d44fd210a111ad9bb8c29278a8c9
Rayendra Sabandar <86864902+RayendraSabandar@users.noreply.github.com>
2025-06-18 15:55:12 +0700

    fix(makefile): removed dotenv usage

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"log"
-
-	"github.com/car-journal/config"
 	"github.com/car-journal/transport/container"
 	"github.com/car-journal/transport/http/server"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	envDir := config.Get(config.ENV_DIR)
-	err := godotenv.Load(envDir)
-	if err != nil {
-		log.Fatalf("Some error occured. Err: %s", err)
-	} else {
-		http := server.CreateHttpServer(container.CreateAppContainer())
-		http.Serve()
-	}
+	http := server.CreateHttpServer(container.CreateAppContainer())
+	http.Serve()
 }
