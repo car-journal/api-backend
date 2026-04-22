e202bd0ab3579cfc8297790b18a6223de901132c
Rayendra Sabandar <86864902+RayendraSabandar@users.noreply.github.com>
2026-04-22 17:02:03 +0700

    feat(postgres): added DBSSLMode env var

--- config/postgres.go
+++ config/postgres.go
@@ -20,6 +20,7 @@ const (
 	DBName     = "DB_NAME"
 	DBUser     = "DB_USER"
 	DBPass     = "DB_PASS"
+	DBSSLMode  = "DB_SSL_MODE"
 	Ascending  = "ASC"
 	Descending = "DESC"
 	Random     = "random"
@@ -33,29 +34,32 @@ var postgresConfig = map[string]string{
 	DBName:    "volleyball_db",
 	DBUser:    "postgres",
 	DBPass:    "postgres",
+	DBSSLMode: "disable",
 }
 
 func ConnectGormPostgres() *gorm.DB {
 	dbOnce.Do(func() {
 		var err error
 		conn := fmt.Sprintf(
-			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 			Get(DBHost),
 			Get(DBPort),
 			Get(DBUser),
 			Get(DBPass),
 			Get(DBName),
+			Get(DBSSLMode),
 		)
 		db, err = gorm.Open(postgres.Open(conn), &gorm.Config{})
 		if nil != err {
 			logger.LoggerInterface.Log(err.Error())
 			panic(err)
 		}
-		log.Printf(`Connected to database postgres (%s) at %s:%s with user "%s"`,
+		log.Printf(`Connected to database postgres (%s) at %s:%s with user "%s" and sslmode "%s"`,
 			Get(DBName),
 			Get(DBHost),
 			Get(DBPort),
 			Get(DBUser),
+			Get(DBSSLMode),
 		)
 	})
 
