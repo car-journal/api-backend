db81ad79d15255d8a7d1e3b20d2a749326a8af64
Rayendra Sabandar <86864902+RayendraSabandar@users.noreply.github.com>
2026-04-23 11:59:54 +0700

    seeder(maintenancecategory): added Service category

--- database/seeder/04_maintenance_categories.sql
+++ database/seeder/04_maintenance_categories.sql
@@ -4,7 +4,8 @@ INSERT INTO maintenance_categories (id, name, description, created_at, updated_a
 ('adb4cc0b-5f22-4260-8686-a4dbc9f479cb', 'Support & Fluids', 'Consumable liquids and cooling components that prevent mechanical failure', NOW(), NOW(), NULL),
 ('8568897d-d3f2-465f-abee-09aa9656de49', 'Electrical & Tech', 'Manages the power, lighting, and modern conveniences of the vehicle.', NOW(), NOW(), NULL),
 ('4ce1ef90-3549-4c66-a927-0e29f695510e', 'Body & Exterior', 'Focuses on environmental protection and the physical look of the car.', NOW(), NOW(), NULL),
-('964b5ddb-be72-4792-95ad-93403cc32565', 'Interior & Comfort', 'Everything inside the car that affects your driving experience.', NOW(), NOW(), NULL)
+('964b5ddb-be72-4792-95ad-93403cc32565', 'Interior & Comfort', 'Everything inside the car that affects your driving experience.', NOW(), NOW(), NULL),
+('cffa5633-fc81-4cb3-b1ae-9719f01024d4', 'Service', 'The place you went to to get your vehicle serviced.', NOW(), NOW(), NULL)
 ON CONFLICT (id)
 DO UPDATE SET 
     name = EXCLUDED.name,
